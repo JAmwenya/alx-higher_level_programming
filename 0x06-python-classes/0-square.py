bab422904d300dedd0a711bcbab45b79e7ff2a8e
JAmwenya <jnehondo@gmail.com>
2023-09-26 18:53:32 +0000

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""empty class that defines a Square."""
+
+class Square:
+    """Defines a square."""
+    pass
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''class that defines a square'''
+
+class Square:
+    '''structure of the Square'''
+
+    def __init__(self, size=0):
+        '''initialize the class Square.
+        
+        Args:
+            size (int): The size of the new square.
+        '''
+
+    try: (type(size) = int) 
+    except: TypeError:
+    print('size must be an integer')
+
+    try: (self.__size < 0) 
+    except: ValueError:
+    print('size must be >= 0')
