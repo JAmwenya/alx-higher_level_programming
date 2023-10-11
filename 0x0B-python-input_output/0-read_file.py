c5c3a321f902778f3a64521a17ea6fcef14f75dd
JAmwenya <jnehondo@gmail.com>
2023-10-11 09:23:03 +0000

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,11 +3,6 @@
 
 
 def read_file(filename=""):
-    """
-    This function reads a text file and prints its contents to stdout.
-
-    Args:
-        filename (str): The name of the file to be read with a default of an empty string.
-    """
+    """This function reads a text file and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
