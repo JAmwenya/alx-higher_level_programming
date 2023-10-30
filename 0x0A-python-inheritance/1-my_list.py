93a579278c2945a077507942dbde18c33c07c353
JAmwenya <jnehondo@gmail.com>
2023-10-30 19:28:43 +0000

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-"""Returns the list of available attributes and methods of an object"""
+"""This is a module that inherits from the list class"""
 
-class InpectObject:
-    """
-    A class that lists available attribute and methods of an object.
 
-    Usage:
-   Create a class instance.
-   Call the `lookup(obj)` method with the object you want to inspect.
-
-    """
-def lookup(self, obj):
-        """
-        Lists available attributes and methods of an object.
-
-        Args:
-        obj: The object to be inspected.
-
-        Returns:
-       	A list of attribute and method names.
-        """
-        return dir(obj)
+class MyList(list):
+    """Class that inherits from list"""
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+class BaseGeometry:
+    """Represents a base geometry class"""
+    
+    def area(self):
+        """Does not implement the method"""
+        raise Exception("area() is not implemented")
