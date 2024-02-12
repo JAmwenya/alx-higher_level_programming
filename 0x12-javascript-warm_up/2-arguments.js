57a75467a83942824d95b510a71085250d4d463e
JAmwenya <jnehondo@gmail.com>
2024-02-12 20:36:10 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argument = prompt("");
+if (argument == 0){
+console.log("No argument");
+}
+else if (argument ==[0]) {
+console.log("Argument found");
+}
+else{
+console.log("Arguments found");
+}
